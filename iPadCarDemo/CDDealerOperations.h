5ad18d4b8bdd8f4c35018884264a966c3d2b7e13
John Basile <numerics77@yahoo.com>
2014-03-25 16:17:13 -0700

    Needed Files to Complete

---
+++ iPadCarDemo/CDDealerOperations.h
@@ -0,0 +1,22 @@
+//
+//  CDDealerOperations.h
+//  iPadCarDemo
+//
+//  Created by John Basile on 2/18/14.
+//  Copyright (c) 2014 Numerics. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CDDealerOperations : NSObject
+
+@property (nonatomic, strong) NSString		*Wednesday;
+@property (nonatomic, strong) NSString		*Tuesday;
+@property (nonatomic, strong) NSString		*Thursday;
+@property (nonatomic, strong) NSString		*Saturday;
+@property (nonatomic, strong) NSString		*Friday;
+@property (nonatomic, strong) NSString		*Monday;
+@property (nonatomic, strong) NSString		*Sunday;
+
+
+@end
---
+++ iPadCarDemo/CDDealerOperations.m
@@ -0,0 +1,18 @@
+//
+//  CDDealerOperations.m
+//  iPadCarDemo
+//
+//  Created by John Basile on 2/18/14.
+//  Copyright (c) 2014 Numerics. All rights reserved.
+//
+
+#import "CDDealerOperations.h"
+
+@implementation CDDealerOperations
+
+- (NSString *)debugDescription;
+{
+	return @"operations";
+}
+
+@end
